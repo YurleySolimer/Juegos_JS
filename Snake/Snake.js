2656f60adad8e22a1bfd73a745cb9d75fc0dc96b
YurleySolimer <yurley.solimer@gmail.com>
2019-01-22 10:50:51 -0400

    Agregando clase objeto

--- Snake/Snake.js
+++ Snake/Snake.js
@@ -1,7 +1,26 @@
 alert("FFFFFFFF");
 var velocidad = 100;
-var tamaño = 40;
+var tamano = 40;
 
+class objeto {
+	constructor () {
+		this.tamano = tamano;
+	}
+
+	choque(obj) {
+		var difx = Math.abs(this.x - obj.x);
+		var dify = Math.abs(this.y - obj.y);
+
+		if (difx >=0 && difx <tamano && dify >=0 && dify <tamano )
+		{
+			return true;
+		}
+		else
+		{
+			return false; 
+		}
+	}
+}
 
 
 function dibujando () {
@@ -9,7 +28,7 @@ function dibujando () {
 	var lienzo= d.getContext("2d");
 
 	lienzo.clearRect(0, 0, juego.width, juego.height);
-	lienzo.fillRect(10, 10, 100, 100);
+	
 }
 
 function main () {
