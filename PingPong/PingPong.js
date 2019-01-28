d07bdb42fc94f20eee6c5b825d4e4bbfa370e43b
YurleySolimer <yurley.solimer@gmail.com>
2019-01-28 19:00:48 -0400

    Funciones globales

--- PingPong/PingPong.js
+++ PingPong/PingPong.js
@@ -6,3 +6,17 @@ var areaH = canvas.height;
 var ctx = canvas.getContext ("2d");
 var puntosj1 = 0;
 var puntosj2 = 0; 
+
+function dibujar () {
+	ctx.clearRect (0, 0, areaW, areaH);
+
+}
+
+function frame () {
+	requestAnimationFrame(frame);
+
+}
+
+function iniciar () {
+	frame ();
+}
