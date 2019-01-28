3ee8b937eb8fd5eff82400954f14705786434298
YurleySolimer <yurley.solimer@gmail.com>
2019-01-28 18:57:48 -0400

    Variables globales

---
+++ PingPong/PingPong.js
@@ -0,0 +1,8 @@
+var bucle;
+var velocidad = 5;
+var canvas = document.getElementById('canvas');
+var areaW = canvas.width;
+var areaH = canvas.height;
+var ctx = canvas.getContext ("2d");
+var puntosj1 = 0;
+var puntosj2 = 0; 
