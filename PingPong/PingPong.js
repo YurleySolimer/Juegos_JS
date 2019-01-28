26cc6c4d09f5fe9d47d7cac759025b8698748873
YurleySolimer <yurley.solimer@gmail.com>
2019-01-28 19:05:01 -0400

    clase base

--- PingPong/PingPong.js
+++ PingPong/PingPong.js
@@ -7,13 +7,26 @@ var ctx = canvas.getContext ("2d");
 var puntosj1 = 0;
 var puntosj2 = 0; 
 
+class Base {
+	choque (obj) {
+		if (this.fondo < obj.y || this.y > obj.fondo || this.derecha < obj.x || this.x > obj.derecha) {
+			return false;
+		}
+
+		return {
+			return true;
+		}
+	}
+}
+
+
 function dibujar () {
 	ctx.clearRect (0, 0, areaW, areaH);
 
 }
 
 function frame () {
-	requestAnimationFrame(frame);
+	bucle = requestAnimationFrame(frame);
 
 }
 
