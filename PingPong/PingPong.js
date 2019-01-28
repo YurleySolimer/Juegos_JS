134cd14f74b1f4d85d94b1d12ea857f87c945524
YurleySolimer <yurley.solimer@gmail.com>
2019-01-28 19:14:34 -0400

    clase bola

--- PingPong/PingPong.js
+++ PingPong/PingPong.js
@@ -19,6 +19,43 @@ class Base {
 	}
 }
 
+class Bola extends Base {
+	constructor () {
+		super ();
+		this.t = 25;
+		this.x = Math.floor (Math.random() * (areaW - this.t));
+		this.x = Math.floor (Math.random() * (areaH - this.t));
+		this.xdir = velocidad;
+		this.ydir = velocidad;
+	}
+
+	choqueV () {
+		if (this.y <= 0 || this.y >= areaH - this.t) {
+			this.ydir = -this.ydir; 
+		}
+	}
+
+	choqueH () {
+		if (this.xdir <= 0) {
+			this.xdir = -this.xdir;
+			puntosj2 ++;
+		}
+
+		if (this.x >= (areaW - this.t)) {
+			this.xdir = -this.xdir;
+			puntosj1 ++;
+		}
+
+	}
+
+	mover () {
+		this.x += this.xdir;
+		this.y += this.ydir;
+		this.choqueV ();
+		this.choqueH ();
+	}
+}
+
 
 function dibujar () {
 	ctx.clearRect (0, 0, areaW, areaH);
