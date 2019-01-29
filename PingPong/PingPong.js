b8a1796573557c6a993b5b25b419f3637427a0e1
YurleySolimer <yurley.solimer@gmail.com>
2019-01-29 11:03:36 -0400

    agregando funciones

--- PingPong/PingPong.js
+++ PingPong/PingPong.js
@@ -1,4 +1,3 @@
-//Variables globales
 var bucle;
 var velocidad = 5;
 var canvas = document.getElementById("canvas");
@@ -9,12 +8,15 @@ var puntosj1 = 0;
 var puntosj2 = 0;
 var tamanoPaleta = 75;
 var superficie = areaH-tamanoPaleta;
-//Clases
+
+
 class Base {
-	choque(obj){
+	choque (obj) {
 		if(this.fondo < obj.y || this.y > obj.fondo || this.derecha < obj.x || this.x > obj.derecha){
 			return false;
-		} else {
+		} 
+
+		else {
 			return true;
 		}
 	}
@@ -25,34 +27,35 @@ class Puntos {
 		this.y = 25;
 		this.punto = 0;
 	}
-	dibujar(){
+	dibujar () {
 		ctx.font = "25px Arial";
-		ctx.fillText(this.punto.toString(), this.x, this.y);
+		ctx.fillText (this.punto.toString(), this.x, this.y);
 	}
 }
 class Bola extends Base {
-	constructor(){
-		super();
+	constructor () {
+		super () ;
 		this.t = 25;
-		this.x = Math.floor(Math.random() * (areaW - this.t));
-		this.y = Math.floor(Math.random() * (areaH - this.t));
+		this.x = Math.floor(Math.random () * (areaW - this.t));
+		this.y = Math.floor(Math.random () * (areaH - this.t));
 		this.xdir = velocidad;
 		this.ydir = velocidad;
-		this.p1 = new Puntos(25);
-		this.p2 = new Puntos(575);
+		this.p1 = new Puntos (25);
+		this.p2 = new Puntos (575);
 	}
-	choqueV(){
-		if(this.y <= 0 || this.y >= (areaH -this.t)){
+	choqueV () {
+		if(this.y <= 0 || this.y >= (areaH -this.t)) {
 			this.ydir = -this.ydir;
 		}
 	}
-	choqueH(){
-		if(this.x <= 0){
+	choqueH () {
+		if (this.x <= 0) {
 			this.xdir = -this.xdir;
 			puntosj2++;
 			this.p2.punto = puntosj2;
 		}
-		if(this.x >= (areaW - this.t)){
+
+		if (this.x >= (areaW - this.t)) {
 			this.xdir = -this.xdir;
 			puntosj1++;
 			this.p1.punto = puntosj1;
